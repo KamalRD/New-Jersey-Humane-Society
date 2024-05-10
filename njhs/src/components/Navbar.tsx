7629a65acba785dc27b0cae18fe6b3f608474d73
Kamal RD <imkamaljay@gmail.com>
2024-05-10 07:27:10 -0400

    Second Commit:
    - Recreate Project Structure w/ TS and Tailwind

---
+++ njhs/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div className='w-full h-20 bg-black'>
+        <div className='logoContainer'>
+            <a href='/'>
+                <image></image>
+            </a>
+        </div>
+    </div>
+  )
+}
